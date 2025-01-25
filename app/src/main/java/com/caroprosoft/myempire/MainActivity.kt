a55c611701f168fa8e89a56304a4d5e50ddd6610
dmitry.knyazev <knyazev3679@yandex.ru>
2025-01-25 23:14:14 +0300

    adding Fragment navigation

--- app/src/main/java/com/caroprosoft/myempire/MainActivity.kt
+++ app/src/main/java/com/caroprosoft/myempire/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.caroprosoft.myempire
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonHome: ImageView
+    lateinit var buttonEconomic: ImageView
+    lateinit var buttonResearch: ImageView
+    lateinit var buttonArmy: ImageView
+    lateinit var buttonWorld: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        init()
+    }
+
+    fun init() {
+        supportFragmentManager.beginTransaction().replace(R.id.place_holder, HomeFragment.newInstance()).commit()
+        buttonHome = findViewById(R.id.button_home)
+        buttonEconomic = findViewById(R.id.button_economic)
+        buttonResearch = findViewById(R.id.button_research)
+        buttonArmy = findViewById(R.id.button_army)
+        buttonWorld = findViewById(R.id.button_world)
+        buttonHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.place_holder, HomeFragment.newInstance()).commit()
+        }
+        buttonEconomic.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.place_holder, EconomicFragment.newInstance()).commit()
+        }
+        buttonResearch.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.place_holder, ResearchFragment.newInstance()).commit()
+        }
+        buttonArmy.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.place_holder, ArmyFragment.newInstance()).commit()
+        }
+        buttonWorld.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.place_holder, WorldFragment.newInstance()).commit()
         }
     }
 }
